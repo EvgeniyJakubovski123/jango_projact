322d1eb090c62ed3af5e57e863cd0489fff5c30d
Ugin Jakubovski <jakubovski.jenia@gmail.com>
2025-11-09 15:43:25 +0100

    final commit

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('article/<int:article_id>/', views.article_detail, name='article_detail'),
     path('articles/', views.all_articles, name='all_articles'),
+    path('categories/', views.categories_list, name='categories_list'),
 ]
--- app/blog/views.py
+++ app/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Article
+from .models import Article,Category
 
 def home(request):
     latest_articles = Article.objects.all().order_by('-publication_date')[:3]
@@ -7,7 +7,15 @@ def home(request):
 
 def article_detail(request, article_id):
     article = get_object_or_404(Article, id=article_id)
-    return render(request, 'blog/article_detail.html', {'article': article})
+    comments = article.comment_set.all().order_by('publication_date')  # по дате по возрастанию
+    return render(request, 'blog/article_detail.html', {
+        'article': article,
+        'comments': comments
+    })
 def all_articles(request):
     articles = Article.objects.filter(is_published=True).order_by('-publication_date')
     return render(request, 'blog/all_articles.html', {'articles': articles})
+
+def categories_list(request):
+    categories = Category.objects.all()
+    return render(request, 'blog/categories_list.html', {'categories': categories})
