d8621b4717c036d0f1bcddf2fa45576ec16f6347
Ugin Jakubovski <jakubovski.jenia@gmail.com>
2025-11-09 15:06:43 +0100

    tag 8

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-
-    path( '',views.index,name='index'),
+    path('', views.home, name='home'),
+    path('article/<int:article_id>/', views.article_detail, name='article_detail'),
 ]
--- app/blog/views.py
+++ app/blog/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Article
 
-# Create your views here.
-def index(request):
-    return render(request, 'blog/index.html')
+def home(request):
+    latest_articles = Article.objects.filter(is_published=True).order_by('-publication_date')[:3]
+    return render(request, 'blog/home.html', {'latest_articles': latest_articles})
+
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    return render(request, 'blog/article_detail.html', {'article': article})
