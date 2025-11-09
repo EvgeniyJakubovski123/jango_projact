62b64d06caac1c5d6454d35fef79bfd1a726eb04
Ugin Jakubovski <jakubovski.jenia@gmail.com>
2025-11-09 15:29:28 +0100

    9 tag

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('article/<int:article_id>/', views.article_detail, name='article_detail'),
+    path('articles/', views.all_articles, name='all_articles'),
 ]
--- app/blog/views.py
+++ app/blog/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 def home(request):
-    latest_articles = Article.objects.filter(is_published=True).order_by('-publication_date')[:3]
+    latest_articles = Article.objects.all().order_by('-publication_date')[:3]
     return render(request, 'blog/home.html', {'latest_articles': latest_articles})
 
 def article_detail(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     return render(request, 'blog/article_detail.html', {'article': article})
+def all_articles(request):
+    articles = Article.objects.filter(is_published=True).order_by('-publication_date')
+    return render(request, 'blog/all_articles.html', {'articles': articles})
