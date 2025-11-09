74d1c36e30c0a8207f575e25ef1dd4f5e7897889
Ugin Jakubovski <jakubovski.jenia@gmail.com>
2025-11-09 14:39:39 +0100

    better admin

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Category, Tag, Article, Comment
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "icon")
+    search_fields = ("title",)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    search_fields = ("title",)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "category", "publication_date", "is_published")
+    list_filter = ("category", "is_published", "publication_date")
+    search_fields = ("title", "author", "text")
+    filter_horizontal = ("tag",)
+    date_hierarchy = "publication_date"
+    ordering = ("-publication_date",)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("author", "article", "publication_date")
+    search_fields = ("author", "text")
+    list_filter = ("publication_date",)
--- app/blog/models.py
+++ app/blog/models.py
@@ -2,40 +2,30 @@ from django.db import models
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-    icon = models.CharField(max_length=255, blank=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    icon = models.CharField(max_length=50)
 
     def __str__(self):
         return self.title
 
 
 class Tag(models.Model):
-    title = models.CharField(max_length=100, unique=True)
+    title = models.CharField(max_length=50)
 
     def __str__(self):
         return self.title
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
     text = models.TextField()
-    image = models.CharField(max_length=255, blank=True)
+    image = models.URLField()  # ссылка на картинку
     publication_date = models.DateField()
     is_published = models.BooleanField(default=False)
-
-    category = models.ForeignKey(
-        Category,
-        on_delete=models.CASCADE,
-        related_name='articles'
-    )
-
-    tag = models.ManyToManyField(
-        Tag,
-        related_name='articles',
-        blank=True
-    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return self.title
@@ -43,13 +33,10 @@ class Article(models.Model):
 
 class Comment(models.Model):
     text = models.TextField()
-    author = models.CharField(max_length=255)
+    author = models.CharField(max_length=100)
     publication_date = models.DateField()
-    article = models.ForeignKey(
-        Article,
-        on_delete=models.CASCADE,
-        related_name='comments'
-    )
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'Comment by {self.author} on {self.article}'
+        return f"Comment by {self.author} on {self.article.title}"
+
